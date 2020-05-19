4afa790c2f929a44cd35bde13b8504a532c77d93
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-05-18 21:55:20 -0700

    Implemented a drop down menu

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let dropBtn = document.querySelector(".dropBtn");
+let menu = document.querySelector(".dropdown ul")
+document.addEventListener('click', function(){
+    menu.classList.toggle("hide");
+})
