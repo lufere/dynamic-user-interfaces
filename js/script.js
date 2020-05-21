2cf11c0b580be6a2b3e9830e422946499fce0a69
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-05-20 18:25:59 -0700

    Finished the progressively collapsing menu now the pages renders a number of navigation options based on the width and stores the rest on the more tab

--- js/script.js
+++ js/script.js
@@ -1,44 +1,63 @@
-let pageWidth = window.innerWidth;
 
 let sections = ["Home", "News", "Sports", "Reel", "Worklife", "Travel", "Future", "Culture", "Music", "Tv", "Weather", "Sounds"];
 
+
 // for (let section of sections){
-for (let i = 0 ; i<5 ; i++){
-    let categoryContainer = document.createElement("div");
-    categoryContainer.classList.add("navItem");
-    let categoryLink = document.createElement("a");
-    categoryContainer.appendChild(categoryLink);
-    categoryLink.innerHTML = sections[i];
-    document.querySelector("header").appendChild(categoryContainer);
+
+function renderNav(){
+document.querySelector("header").innerHTML = "";
+let pageWidth = window.innerWidth;
+let navWidth = pageWidth - (pageWidth*0.4);
+let showQty = Math.floor(navWidth/100);
+// alert(navWidth);
+if (showQty > 9) showQty = 9;
+if (showQty < 1) showQty = 1;
+// alert(showQty);  
+    for (let i = 0 ; i<showQty ; i++){
+        let categoryContainer = document.createElement("div");
+        categoryContainer.classList.add("navItem");
+        let categoryLink = document.createElement("a");
+        categoryContainer.appendChild(categoryLink);
+        categoryLink.innerHTML = sections[i];
+        document.querySelector("header").appendChild(categoryContainer);
+    }
+    let dropdown = document.createElement("div");
+    dropdown.classList.add("dropdown");
+    let dropLabel = document.createElement("p");
+    dropLabel.innerHTML = "More";
+    dropLabel.classList.add("dropBtn");
+    let list = document.createElement("ul");
+    list.classList.add("hide");
+    for (let i = showQty ; i<sections.length ; i++){
+        let test = document.createElement("a");
+        test.innerHTML = sections[i];
+        list.appendChild(test);
+    }
+    dropdown.appendChild(dropLabel);
+    dropdown.appendChild(list);
+    document.querySelector("header").appendChild(dropdown);
+    // alert(window.innerWidth);
+    setListeners();
 }
-let dropdown = document.createElement("div");
-dropdown.classList.add("dropdown");
-let dropLabel = document.createElement("p");
-dropLabel.innerHTML = "More";
-dropLabel.classList.add("dropBtn");
-let list = document.createElement("ul");
-list.classList.add("hide");
-let test = document.createElement("a");
-test.innerHTML = "works";
-list.appendChild(test);
-dropdown.appendChild(dropLabel);
-dropdown.appendChild(list);
-document.querySelector("header").appendChild(dropdown);
-// alert(window.innerWidth);
 
-let menu = document.querySelector(".dropdown ul");
+function setListeners(){
+    let menu = document.querySelector(".dropdown ul");
 
+    let dropBtn = document.querySelector(".dropBtn");
+    dropdown = document.querySelector("dropdown")
+    dropBtn.addEventListener('mouseenter', function(){
+        menu.classList.toggle("hide");
+    })
 
-let dropBtn = document.querySelector(".dropBtn");
-dropdown = document.querySelector("dropdown")
-dropBtn.addEventListener('mouseenter', function(){
-    menu.classList.toggle("hide");
-})
+    dropBtn.addEventListener('mouseleave', function(){
+        menu.classList.toggle("hide");
+    })
 
-dropBtn.addEventListener('mouseleave', function(){
-    menu.classList.toggle("hide");
-})
+    menu.addEventListener('mouseleave', function(){
+        menu.classList.toggle("hide");
+    })
 
-menu.addEventListener('mouseleave', function(){
-    menu.classList.toggle("hide");
-})
+}
+
+renderNav();
+window.addEventListener('resize', renderNav);
